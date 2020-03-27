7bcf82bee26a9783f07f0b82629e8ad2aab94efe
m hos <mhosain487@gmail.com>
2020-03-27 15:13:07 -0400

    V

---
+++ t/T.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace t
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class T : ContentPage
+    {
+        public T()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "Ok");
+        }
+    }
+}
